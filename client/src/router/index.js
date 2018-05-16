3e82da1e58545e46b89929ed7b129466d89a6220
neo <jbruell94@gmail.com>
2018-05-16 08:17:03 -0400

    added new client

--- client/src/router/index.js
+++ client/src/router/index.js
@@ -2,6 +2,7 @@ import Vue from 'vue'
 import Router from 'vue-router'
 import Manager from '@/components/Manager'
 import nomslyClient from '@/components/nomslyClient'
+import Client from '@/components/Client'
 import editAccount from '@/components/managerViews/editAccount'
 import editMeal from '@/components/managerViews/editMeal'
 
@@ -18,6 +19,11 @@ export default new Router({
       path: '/client/:accountNumber',
       name: 'nomslyClient',
       component: nomslyClient
+    },
+    {
+      path: '/NewClient/:accountNumber',
+      name: 'Client',
+      component: Client
     },
     {
       path: '/editAccount/:accountNumber',
